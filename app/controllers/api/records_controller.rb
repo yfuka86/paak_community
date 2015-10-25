dcd2871d671ab24432536debfd3e43334a2766a8
yfuka86 <yfuka86@gmail.com>
2015-10-25 15:53:33 +0900

    fix latest_ts

--- app/controllers/api/records_controller.rb
+++ app/controllers/api/records_controller.rb
@@ -1,7 +1,7 @@
 class Api::RecordsController < Api::BaseController
   def latest_ts
     render json: {
-      ts:Record.order(timestamp: :desc).first.timestamp.to_i
+      ts: Record.order(timestamp: :desc).first.try(:timestamp).try(:to_i) || 1
       }.to_json
   end
 end
