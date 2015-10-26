3b0be7fb72971d63db475854f3db387f518c95ba
yfuka86 <yfuka86@gmail.com>
2015-10-26 14:15:04 +0900

    extract html link in text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def text_url_to_link(text)
+    URI.extract(text, ['http','https']).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " >" << url << "</a>"
+      text.gsub!(url, sub_text)
+    end
+    return text
+  end
 end
