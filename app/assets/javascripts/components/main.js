e877555e43320dddb2e976e4c3a2858683ab8365
yfuka86 <yfuka86@gmail.com>
2015-10-26 23:43:39 +0900

    fix design

--- app/assets/javascripts/components/main.js
+++ app/assets/javascripts/components/main.js
@@ -18,11 +18,15 @@ $(function(){
               $('#user-board').append($div);
             }
             res.users.forEach(function(u){
-              $div = $("<a class='user-card' href='" + location.origin + '/users/' + u.id + "'></a>");
-              $image = $("<img class='user-image' src='" + u.image_url + "?width=200&height=200'/>");
+              $link = $("<a class='user-card' href='" + location.origin + '/users/' + u.id + "'></a>");
+              $image = $("<img class='user-image' src='" + u.image_url + "?width=300&height=300'/>");
               $period = $("<p class='user-period'>#" + u.latest_period + "</p>");
-              $name = $("<p class='user-name'>" + u.name + "<a href='" + u.facebook_url + "'><span class='fa fa-facebook' /></a></p>");
-              $div.append([$image, $period, $name]);
+              $name = $("<p class='user-name'>" + u.name + "</p>");
+              $facebook = $("<p><a href='" + u.facebook_url + "'><span class='fa fa-facebook' /></a></p>")
+              $link.append([$image, $period, $name, $facebook]);
+              $div = $("<div class='user-card-container col-xs-6 col-sm-3 col-md-3 col-lg-2'></div>");
+              $div.append($link);
+
               $('#user-board').append($div);
             })
           });
