0d41170555c635abc6ecdf902b65b460f0958f83
yfuka86 <yfuka86@gmail.com>
2015-10-27 12:38:22 +0900

    hotfix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
   before_action :set_options, only: [:new, :edit, :create, :update]
-  before_action :authenticate_member!
+  before_action :authenticate_project_member!, only: [:edit, :update, :destroy]
 
   # GET /projects
   # GET /projects.json
