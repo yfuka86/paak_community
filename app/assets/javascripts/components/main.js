bc26d17f43845b72a7401d006cffa5c27b3e99ca
yfuka86 <yfuka86@gmail.com>
2015-11-02 18:26:28 +0900

    install masonry

--- app/assets/javascripts/components/main.js
+++ app/assets/javascripts/components/main.js
@@ -22,19 +22,21 @@ function reload () {
         if (res.users.length === 0) {
           $div = $("<div class='no-user'>No members to show</div>");
           $('#user-board').append($div);
+        } else {
+          res.users.forEach(function(u){
+            $link = $("<a class='user-card' href='" + location.origin + '/users/' + u.id + "'></a>");
+            $image = $("<img class='user-image' src='" + u.image_url + "?width=300&height=300'/>");
+            $period = $("<p class='user-period'>#" + u.latest_period + "</p>");
+            $name = $("<p class='user-name'>" + u.name + "</p>");
+            $facebook = $("<p><a href='" + u.facebook_url + "'><span class='fa fa-facebook' /></a></p>")
+            $link.append([$image, $period, $name, $facebook]);
+            $('#user-board').append($link);
+          })
+          $('#user-board').masonry({
+            itemSelector: '.user-card',
+            columnWidth: 200
+          });
         }
-        res.users.forEach(function(u){
-          $link = $("<a class='user-card' href='" + location.origin + '/users/' + u.id + "'></a>");
-          $image = $("<img class='user-image' src='" + u.image_url + "?width=300&height=300'/>");
-          $period = $("<p class='user-period'>#" + u.latest_period + "</p>");
-          $name = $("<p class='user-name'>" + u.name + "</p>");
-          $facebook = $("<p><a href='" + u.facebook_url + "'><span class='fa fa-facebook' /></a></p>")
-          $link.append([$image, $period, $name, $facebook]);
-          $div = $("<div class='user-card-container col-xs-6 col-sm-3 col-md-3 col-lg-3'></div>");
-          $div.append($link);
-
-          $('#user-board').append($div);
-        })
       });
 
       timestamp = ts;
