f170af8082dbf49ab55878c93aaa6a154dc9d3a5
garasubo <garasubo@gmail.com>
2016-02-04 19:43:11 +0900

    fix date

--- app/assets/javascripts/components/main.js
+++ app/assets/javascripts/components/main.js
@@ -55,7 +55,7 @@ function formatDate(date) {
   var format = "YYYY年MM月DD日";
   format = format.replace(/YYYY/g, date.getFullYear());
   format = format.replace(/MM/g, ('0'+(date.getMonth()+1)).slice(-2));
-  format = format.replace(/DD/g, ('0'+(date.getDate()+1)).slice(-2));
+  format = format.replace(/DD/g, ('0'+(date.getDate())).slice(-2));
   return format;
 }
 
